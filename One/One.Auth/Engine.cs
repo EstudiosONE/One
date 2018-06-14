09ef462a53e9f276a7206b632b9e31c317c85a5d
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-06-14 12:18:15 -0300

    Creación de One.Auth.Core y One.Auth.Engine, definición de metodos en One.Auth.Engine

---
+++ One/One.Auth/Engine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.Auth
+{
+    public class Engine
+    {
+        public static void LogIn(string user, string password)
+        {
+            // Verificar usuario y contraseña.
+            // {code}
+        }
+        public static void LogIn(string token)
+        {
+            // Verificar token.
+            // {code}
+        }
+        public static void SetPin(string jwt, short pin)
+        {
+
+        }
+        public static void Refresh(string jwt)
+        {
+
+        }
+        public static void Verify(string jwt)
+        {
+
+        }
+        public static void LogOut(string jwt)
+        {
+
+        }
+    }
+}
