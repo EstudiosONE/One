fd0eda2c60c36987df20fe52c6d870bca97290af
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-06-15 20:26:30 -0300

    [CREADO] One.Core.Storage.Location
    Se crea la base del directorio de carpetas donde se van a ubicar los archivos del sistema y configuraciones

---
+++ One.Core.Storage/Locations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.Core.Storage
+{
+    public class Locations
+    {
+        internal string CommonApplicationData { get => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "Estudios One", "iGS"); }
+
+        public Server Server { get; }
+
+        public Locations()
+        {
+            Server = new Server(this);
+        }
+
+
+    }
+
+    public class Server
+    {
+        private Locations Locations { get; }
+
+        internal Server(Locations locations)
+        {
+            Locations = locations;
+        }
+
+        public string CommonApplicationData { get => Path.Combine(Locations.CommonApplicationData, "Server"); }
+
+    }
+}
