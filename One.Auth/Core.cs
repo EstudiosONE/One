1b429d26134ab79c5d9ef531b51adf75178b7293
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-06-15 11:49:45 -0300

    Modificaciones varias

--- One.Auth/Core.cs
+++ One.Auth/Core.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,34 @@ namespace One.Auth
 {
     internal partial class Core
     {
+        private byte[] Secret { get; set; }
 
+        public Core(string secret)
+        {
+            Secret = Convert.FromBase64String(secret);
+        }
+
+        public string GenerateToken(string username, int expireMinutes = 20)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Secret), SecurityAlgorithms.HmacSha256Signature),
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
     }
 }
---
+++ One.Auth/Core/Generation/GenerateSessionToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.Auth
+{
+    internal partial class Core
+    {
+        internal void GenerateSessionToken()
+        {
+            // verifico usuario y contraseña
+
+                // devuelvo null por datos invalidos
+
+            // genero el token
+
+            // devuelvo el token
+        }
+
+    }
+}
