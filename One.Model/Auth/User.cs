ebe20d51537de39991777040aca378a53e0c15ea
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-06-15 13:13:01 -0300

    [MODIFICADO] One.Model.Auth.User
    Se modifica para empezar a usar la clase base [Table]

--- One.Model/Auth/User.cs
+++ One.Model/Auth/User.cs
@@ -13,34 +13,33 @@ using System.Reflection;
 namespace One.Model.Auth
 {
     [Table(Name = "Auth.[User]")]
-    public partial class User
+    public partial class User : Misc.Table
     {
-        private string _Id;
-        private int _PaxCod;
-        private string _State;
+        private string _Person;
+        private StateName _State;
+
+        public enum StateName
+        {
+            Pendiente = 0, Activo = 1, Suspendido = 3, Cancelado = 9
+        }
 
         public User()
         {
             _Id = LiteDB.ObjectId.NewObjectId().ToString();
+            _CreationDate = DateTime.Now;
         }
 
-        [Column(Storage = "_Id", DbType = "NChar(24)", CanBeNull = false, IsPrimaryKey = true)]
-        public string Id
+        [Column(Storage = "_Person", DbType = "NChar(24)", CanBeNull = false)]
+        public string Person
         {
-            get => _Id;
-            set { if ((_Id != value)) Id = value; }
+            get => _Person;
+            set { if ((Person != value)) Modify(value, out _Person); }
         }
-        [Column(Storage = "_PaxCod", DbType = "Int", CanBeNull = false)]
-        public int PaxCod
-        {
-            get => _PaxCod;
-            set { if ((PaxCod != value)) _PaxCod = value; }
-        }
-        [Column(Storage = "_State", DbType = "NVarChar(50)")]
-        public string State
+        [Column(Storage = "_State", DbType = "NVarChar(24)")]
+        public StateName State
         {
             get => _State;
-            set { if ((_State != value)) _State = value; }
+            set { if ((_State != value)) Modify(_State, out value); }
         }
     }
 }
