ef9cd1973e56559c708bd6fa1a869d81289c1f5f
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-06-15 16:45:11 -0300

    [CREADO] One.Model.Auth.UserSecret
    Se crea dicha clase para manejar las claves de usuarios del sistema. Las mismas mantienen un historial de claves para dar soporte a las politicas de seguridad que se crearían en un futuro

---
+++ One.Model/Auth/UserSecret.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One.Model.Auth
+{
+    [Table(Name = "Auth.[UserSecret]")]
+    public class UserSecret : Misc.Table
+    {
+        bool _IsActualSecret;
+
+        // Propiedades de tabla
+        [Column(Storage = "_ModificationDate", DbType = "datetime", CanBeNull = false, IsPrimaryKey = true)]
+        public new DateTime ModificationDate
+        {
+            get => _ModificationDate;
+        }
+        [Column(DbType = "nchar(24)", CanBeNull = false)]
+        public string User { get; }
+        [Column(DbType = "binary", CanBeNull = false)]
+        public byte[] Secret { get; }
+        [Column(Storage = "_IsActualSecret", DbType = "bit", CanBeNull = false)]
+        public bool IsActualSecret
+        {
+            get => _IsActualSecret;
+            set
+            {
+                if (value != IsActualSecret & !value) Modify(value, out _IsActualSecret);
+                else if (value) throw new Misc.ModelException(Misc.ModelException.ErrorCodeName.Auth_UserSecret_ReactiveSecret);
+            }
+        }
+
+        // Constructores
+        internal UserSecret() : base() { _IsActualSecret = true; }
+        public UserSecret(string user, byte[] secret) : base()
+        {
+            _IsActualSecret = true;
+            User = user;
+            Secret = secret;
+        }
+
+        // Metodos
+    }
+}
