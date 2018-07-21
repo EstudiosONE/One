f510ef95b1f036c446236754ef26ab3e37fb66c2
iGSUruguay <30572826+iGSUruguay@users.noreply.github.com>
2018-07-21 13:34:43 -0300

    Varios

--- One.Core.Auth/Service.cs
+++ One.Core.Auth/Service.cs
@@ -21,7 +21,7 @@ namespace One.Core.Auth
 
         public bool Validation(System.Net.Http.HttpRequestMessage request, string operation)
         {
-
+            var a = Operation.Hotel.Reservation.New;
 
             return true;
         }
@@ -29,6 +29,38 @@ namespace One.Core.Auth
 
 
 
-        public struct 
+        public struct Operation
+        {
+            private readonly short 
+                _module, 
+                _subModule, 
+                _operation, 
+                _subOperation;
+
+            public Operation(short module, short subModule, short operation, short subOperation)
+            {
+                _module = module;
+                _subModule = subModule;
+                _operation = operation;
+                _subOperation = subOperation;
+            }
+
+            public override string ToString()
+            {
+                return $"{_module.ToString("0000")}.{_subModule.ToString("0000")}.{_operation.ToString("0000")}.{_subOperation.ToString("0000")}";
+            }
+
+            public static class Hotel
+            {
+                internal static short Current => 0100;
+                
+                public static class Reservation
+                {
+                    internal static short Current => 0001;
+
+                    public static Operation New => new Operation(Hotel.Current, Reservation.Current, 0500, 0000);
+                }
+            }
+        }
     }
 }
